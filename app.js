6e08b7c7c9a538568e9eace0079f079c9d733e09
Dresosh <contact@dresosh.com>
2015-08-21 19:56:28 -0700

    taking out code repetition and refining js

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var button = document.querySelectorAll(".button");
+var display = document.querySelector("#display");
+var memory = [];
 
-for( i=0 ; i < button.length ; i++ ){
+for( var i = 0 ; i < button.length ; i++ ){
 	button[i].addEventListener("click", clickKey);
 }
 
 function clickKey (){
-	var btnVal = this.getAttribute("date-value");
+	var btnVal = this.getAttribute("data-value");
+	// console.log(btnVal);
+	if (this.getAttribute("data-type") == "operator") {
+		// console.log("NUMBER")
+		display.innerText += btnVal;
+	}
+
+	if (this.getAttribute("data-type") == "evaluator") {
+		// console.log("EVALUATE")
+		display.innerText = eval(display.innerText)
+		// clearDispl();		
+	};
+
+	if(this.getAttribute("data-type") == "clear") {
+		// console.log("CLEAR")
+		clearDispl();
+	}
+
+	if(this.getAttribute("data-type") == "sqrt") {
+		memory.push(display.innerText);
+		var x = memory.pop();
+		// console.log("x is " + x);
+		display.innerText = Math.sqrt(x);
+	}
+}
+
+
+function clearDispl(){
+	display.innerText = "";
 }
